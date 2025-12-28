3d158bfc9ba6fbc84c9da819a5c63d440f41143e
cicadaclock <cicadathon@gmail.com>
2025-12-27 22:38:28 -0500

    Formatting

--- internal/pkg/data/data_test.go
+++ internal/pkg/data/data_test.go
@@ -16,8 +16,28 @@ func setup(t *testing.T) *DataStore {
 func TestFactorNames(t *testing.T) {
 	dataStore := setup(t)
 
-	factorIds := []int{303, 3202, 1000401, 1001101, 2003501, 2004901, 2010503, 2011601, 2015603}
-	want := []string{"Power", "Mile", "Oka Sho", "Yasuda Kinen", "Corner Recovery ○", "Nimble Navigator", "Shifting Gears", "Murmur", "Lucky Seven"}
+	factorIds := []int{
+		303,
+		3202,
+		1000401,
+		1001101,
+		2003501,
+		2004901,
+		2010503,
+		2011601,
+		2015603,
+	}
+	want := []string{
+		"Power ★★★",
+		"Mile ★★",
+		"Oka Sho ★",
+		"Yasuda Kinen ★",
+		"Corner Recovery ○ ★",
+		"Nimble Navigator ★",
+		"Shifting Gears ★★★",
+		"Murmur ★",
+		"Lucky Seven ★★★",
+	}
 	result := dataStore.MapFactorNames(factorIds)
 	if !reflect.DeepEqual(result, want) {
 		t.Errorf("factor ids: %v\nGot: %v\nWant: %v", factorIds, result, want)
